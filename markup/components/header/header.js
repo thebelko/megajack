a0cad6523c6934e05d7b045b98da3d7abc304606
Olga Odnokolova <inc.belko@gmail.com>
2017-06-13 01:31:54 +0300

    заливка верстки

---
+++ markup/components/header/header.js
@@ -0,0 +1,15 @@
+var btnOpen = document.querySelector('.nav__mob-text');
+var btnClose = document.querySelector('.nav__close-wrap');
+
+var menu = document.querySelector('.nav__content');
+
+btnOpen.addEventListener('click', function () {
+    menu.classList.add('nav__content--open');
+    btnClose.classList.add('nav__close-wrap--show');
+});
+
+btnClose.addEventListener('click', function () {
+    menu.classList.remove('nav__content--open');
+    btnClose.classList.remove('nav__close-wrap--show');
+});
+
